beccb5e8ee1e961e7d33f1a93177694dd94e4d24
Johann Roth <johann.roth.e46@googlemail.com>
2024-07-08 16:15:17 +0200

    promise fail test

--- script.js
+++ script.js
@@ -6,17 +6,51 @@ let promError = false;
 function getPromise() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            console.log("1");
             if(promError){
                 reject("hat nicht geklappt");
             }else{
                 resolve("hat geklappt");
             }
-        }, 2000);
+        }, 1000);
+    });
+}
+
+function getPromise2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("2");
+            if(!promError){
+                reject("hat nicht geklappt");
+            }else{
+                resolve("hat geklappt");
+            }
+        }, 1000);
+    });
+}
+
+function getPromise3() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("3");
+            if(promError){
+                reject("hat nicht geklappt");
+            }else{
+                resolve("hat geklappt");
+            }
+        }, 1000);
     });
 }
 
 
 async function usePromise() {
-    let prom = await getPromise();
-    console.log(prom);
+    try {
+        await getPromise();
+        await getPromise2();
+        await getPromise3();
+        console.log("juhuuuu");
+    } catch (error) {
+        console.error(error);
+    }
+    console.log("ende");
 }
