081565c5e6a7c9a2beaaac35cd43c2f313c06a6e
Johann Roth <johann.roth.e46@googlemail.com>
2024-07-14 11:06:28 +0200

    added some Code

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ function init(){
 async function fetchDataJson(){
     let response = await fetch('https://www.fruityvice.com/api/fruit/all');
     let responseAsJson = await response.json();
-    console.log("hat geklappt");
+    console.log("responseAsJson");
 }
 
 function importFruits(){
     fetchDataJson();
+}
+
+function renderListOfFruits(){
+    document.getElementById('content') = '';
+
+    for (let i = 0; i < responseAsJson.length; i++) {
+        let fruits = responseAsJson[i];
+        document.getElementById('content') = /*html*/`
+            <h1>fruits${i}</h1>
+        `
+    }
 }
